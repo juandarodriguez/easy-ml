a27ce8f4c4da9d44bd3eaadb0090e5ad89b4d3c7
Juan David Rodríguez García <juanda@juandarodriguez.es>
2019-04-06 20:23:47 +0200

    funcionando el modal para introducir datos en etiqueta

--- src/app/components/ml-label-container/ml-label-container.component.ts
+++ src/app/components/ml-label-container/ml-label-container.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
-export interface DialogData {
-  animal: string;
-  name: string;
-}
+export type DialogData = string;
 
 @Component({
   selector: 'app-ml-label-container',
@@ -14,52 +11,49 @@ export interface DialogData {
 export class MlLabelContainerComponent implements OnInit {
 
   panelOpenState = true;
-  texts: string[];
+  texts: string[] = [];
 
-  name: string;
-  animal: string;
-
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog) { 
+  }
 
   ngOnInit() {
-    this.texts = [
-      // encender_lampara
-      "que me tocan el culo",
-      "enciende la luz",
-      "esto está muy oscuro",
-      "qué oscuridad",
-      "dale a la luz",
-      "enciende la lámpara",
-      "se está poniendo el sol",
-      "no veo nada",
-      "necesito luz",
-      "no hay suficiente luz",
+    // this.texts = [
+    //   // encender_lampara
+    //   "que me tocan el culo",
+    //   "enciende la luz",
+    //   "esto está muy oscuro",
+    //   "qué oscuridad",
+    //   "dale a la luz",
+    //   "enciende la lámpara",
+    //   "se está poniendo el sol",
+    //   "no veo nada",
+    //   "necesito luz",
+    //   "no hay suficiente luz",
 
-      // apagar_lampara
-      "apaga la luz",
-      "apaga la lámpara",
-      "hay demasiada luz",
-      "menos luz",
-      "desconecta la luz",
-      "quiero estar en la oscuridad",
-      "está demasiado claro",
-      "mucha claridad",
-      "me gusta la oscuridad",
-      "prefiero la oscuridad"
+    //   // apagar_lampara
+    //   "apaga la luz",
+    //   "apaga la lámpara",
+    //   "hay demasiada luz",
+    //   "menos luz",
+    //   "desconecta la luz",
+    //   "quiero estar en la oscuridad",
+    //   "está demasiado claro",
+    //   "mucha claridad",
+    //   "me gusta la oscuridad",
+    //   "prefiero la oscuridad"
 
-    ]
+    // ]
   }
 
   addTerm() {
     const dialogRef = this.dialog.open(MlLabelContainerDialogComponent, {
       width: '250px',
-      data: { name: this.name, animal: this.animal }
+      data: ""
     });
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
-      this.animal = result;
-      alert(result);
+      this.texts.push(result);
     });
   }
 }
